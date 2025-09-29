55cac936c6a4a6a221b8b9a3d6ae1d1c70254d4c
Kayaba Zeref <duyphuong2000.dpp@gmail.com>
2025-09-29 11:07:12 +0700

    Refactor HTML and JavaScript files; add launch configuration

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "Viet Nam";
+let continent = "Asia";
+let population = 97000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+// console.log(firstName);
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+javaScriptIsFun = "YES!";
+console.log(javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
