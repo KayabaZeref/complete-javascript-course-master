ab3e41c4ec9bcb352b09575ebc74e8378880b71f
Kayaba Zeref <duyphuong2000.dpp@gmail.com>
2025-09-30 16:58:00 +0700

    Refactor script.js: Uncomment lastName declaration and add arithmetic operations with console logs

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -42,5 +42,14 @@ console.log(ageJonas, ageSarah);
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 
 const firstName = "Jonas";
-const lastName = "Schmedtmann";
+// const lastName = "Schmedtmann";
+lastName = "Schmedtmann";
 console.log(firstName + " " + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
+console.log(x);
