7a4d6d717cb8bf9dc55157a92f98dc4e0b7ed5a3
Kayaba Zeref <duyphuong2000.dpp@gmail.com>
2025-10-06 15:46:22 +0700

    Refactor challenge3.js to calculate average scores directly; streamline trophy comparison logic

--- 01-Fundamentals-Part-1/starter/challenge3.js
+++ 01-Fundamentals-Part-1/starter/challenge3.js
@@ -2,22 +2,14 @@
 
 // Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
 
-let scoreDolphins;
-let scoreKoalas;
-
-scoreDolphins = 96;
-// scoreDolphins = 108;
-// scoreDolphins = 89;
-
-scoreKoalas = 88;
-// scoreKoalas = 91;
-// scoreKoalas = 110;
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
 
 if (scoreDolphins > scoreKoalas) {
     console.log("Dolphins win the trophy");
 } else if (scoreDolphins < scoreKoalas) {
     console.log("Koalas win the trophy");
-} else {
+} else if (scoreDolphins === scoreKoalas) {
     console.log("Both win the trophy");
 }
 
