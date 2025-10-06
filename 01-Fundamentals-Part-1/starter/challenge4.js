fdaae7c24dd47d49365c0bf3a6ede1023f07fd2d
Kayaba Zeref <duyphuong2000.dpp@gmail.com>
2025-10-06 16:35:51 +0700

    Add switch statement and conditional operator examples in script.js; implement tip calculation in challenge4.js

---
+++ 01-Fundamentals-Part-1/starter/challenge4.js
@@ -0,0 +1,10 @@
+// const bill = 275;
+const bill = 40;
+// const bill = 430;
+
+/* Write your code below. Good luck! 🙂 */
+
+const tip = bill >= 50 && bill <= 300 ? bill * (15 / 100) : bill * (20 / 100);
+
+console.log(`The bill was ${bill}, the tip was ${tip}, 
+and the total value ${bill + tip}`);
