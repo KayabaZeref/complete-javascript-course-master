a6c5174891824cc8f1094d57b8a057b4a87c2ae3
Kayaba Zeref <duyphuong2000.dpp@gmail.com>
2025-11-02 13:19:38 +0700

    Add function examples and logic for average score calculation in challenge1.js; implement winner check for Dolphins and Koalas

---
+++ 02-Fundamentals-Part-2/starter/challenge1.js
@@ -0,0 +1,20 @@
+/* Write your code below. Good luck! 🙂 */
+/* TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
+TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.*/
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log('No team wins...');
+    }
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -40,3 +40,69 @@ const num = Number('23');
 console.log(num);
 
 // 35. Function Declarations vs. Expressions
+// Function Declaration
+// can be called before its declaration
+// const age1 = calcAge1(1991);
+// This works because of hoisting
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+};
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+// 36. Arrow Functions
+// Arrow function expression
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`;
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+
+// 37. Functions Calling Other Functions
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+// const cutFruitPieces = fruit => fruit * 4;
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+// 38. Reviewing Functions
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+};
+const yearsUntilRetirement2 = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years.`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired 🎉`);
+        return -1;
+    }
+}
+
+console.log(yearsUntilRetirement2(1991, 'Jonas'));
+console.log(yearsUntilRetirement2(1950, 'Mike'));
